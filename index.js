d9c52f2b39f05a2371f308b369347c642f5a8808
Garrett <garrettleegrahamanderson@gmail.com>
2023-01-14 17:17:40 -0500

    updated the version on inquirier to 8.2.4. added questions for the inquireer dialogue

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-const fs = require('fs')
 const inquirer = require('inquirer')
+const fs = require('fs')
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project\'s name?',
+        name: 'Project Name'
+    },
+    {
+        type: 'input',
+        message: 'What is the project description?',
+        name: 'Project Description'
+    },
+    {
+        type: 'input',
+        message: 'What are the instructions for installation?',
+        name: 'Installation Instructions'
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'Contribution Guidelines'
+    },
+    {
+        type: 'input',
+        message: 'What are the instructions for testing?',
+        name: 'Test Instructions'
+    },
+    {
+        type: 'list',
+        message: 'What is the project license?',
+        name: 'License',
+        choices: ['None', 'MIT License', 'Mozilla Public License 2.0', 'The Unlicense']
+    }
+]
+
